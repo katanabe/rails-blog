11646b3c88806829b8a9581d5004c900d7571211
katanabe <nabeon+github@gmail.com>
2026-04-28 09:18:43 +0900

    feat: add articles controller and routes

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,55 @@
+class ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: [ :new, :create, :edit, :update, :destroy ]
+  before_action :set_article, only: [ :show, :edit, :update, :destroy ]
+  before_action :authorize_user!, only: [ :edit, :update, :destroy ]
+
+  def index
+    @articles = Article.published
+  end
+
+  def show
+  end
+
+  def new
+    @article = current_user.articles.build
+  end
+
+  def create
+    @article = current_user.articles.build(article_params)
+    if @article.save
+      redirect_to @article, notice: "記事を投稿しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to @article, notice: "記事を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path, notice: "記事を削除しました"
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def authorize_user!
+    redirect_to root_path, alert: "権限がありません" unless @article.user == current_user
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :body, :published_at)
+  end
+end
